38a4536abc7452b82883ec49589b721c92626045
magezil <256@holbertonschool.com>
2017-10-23 20:54:54 +0000

    v2 binary - negative number representation

--- conversions.c
+++ conversions.c
@@ -19,9 +19,37 @@ int print_binary(va_list args)
  */
 int binary(int num)
 {
-	if (num / 2 == 0)
+	int mask = 1;
+	int count = 0;
+	int neg = 0;
+
+	if (num < 0)
+	{
+		num = -num;
+		neg = 1;
+	}
+	while (mask < num)
+	{
+		count++;
+		mask = mask << 1;
+	}
+
+	if (neg)
+		num = -num;
+	else
+		mask = mask >> 1;
+	while (mask != 0)
+	{
+		if ((mask & num) > 0)
+			count += _putchar('1');
+		else
+			count += _putchar('0');
+		mask = mask >> 1;
+	}
+	return count;
+/*	if (num / 2 == 0)
 		return (_putchar(num % 2 + '0'));
 	if (num / 2 == 1)
 		return (_putchar(num / 2 + '0') + _putchar(num % 2 + '0'));
-	return (binary(num / 2) + _putchar(num % 2 + '0'));
+	return (binary(num / 2) + _putchar(num % 2 + '0'));*/
 }
